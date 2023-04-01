b7a01677b1370cb732def6c5c883670021d6ffcc
Yifan Li <yifanl0716@gmail.com>
2023-04-01 19:01:27 -0400

    correct pppm_dplr profiling

--- src/DEEPMD/pppm_dplr.cpp
+++ src/DEEPMD/pppm_dplr.cpp
@@ -3,6 +3,7 @@
 #include <math.h>
 
 #include "atom.h"
+#include "update.h"
 #include "domain.h"
 #include "error.h"
 #include "force.h"
@@ -280,7 +281,7 @@ void PPPMDPLR::compute(int eflag, int vflag) {
   t10 = MPI_Wtime();
   tafter_field_force += (t10-t9);
   ttot += (t10-t1);
-      printf("step = %ld \ntime (s) total: %.4f s \n    before_particle_map | particle_map | make_rho | reverse_comm | brick2fft | poisson | after_poisson | field_force | after_field_force | sum\ntime (s):       %.4f | %.4f | %.4f | %.4f | %.4f | %.4f | %.4f | %.4f | %.4f | %.4f \npercentage (%%) %.4f | %.4f | %.4f | %.4f | %.4f | %.4f | %.4f | %.4f | %.4f | %.4f \n\n", update->ntimestep, ttot, tbefore_particle_map, tparticle_map, tmake_rho, treverse_comm, tbrick2fft, tpoisson, tafter_poisson, tfield_force, tafter_field_force, tbefore_particle_map+tparticle_map+tmake_rho+treverse_comm+tbrick2fft+tpoisson+tafter_poisson+tfield_force+tafter_field_force+, tbefore_particle_map/ttot*100, tparticle_map/ttot*100, tmake_rho/ttot*100, treverse_comm/ttot*100, tbrick2fft/ttot*100, tpoisson/ttot*100, tafter_poisson/ttot*100, tfield_force/ttot*100, tafter_field_force/ttot*100, (tbefore_particle_map+tparticle_map+tmake_rho+treverse_comm+tbrick2fft+tpoisson+tafter_poisson+tfield_force+tafter_field_force)/ttot*100);
+      printf("step = %ld \ntime (s) total: %.4f s \n    before_particle_map | particle_map | make_rho | reverse_comm | brick2fft | poisson | after_poisson | field_force | after_field_force | sum\ntime (s):       %.4f | %.4f | %.4f | %.4f | %.4f | %.4f | %.4f | %.4f | %.4f | %.4f \npercentage (%%) %.4f | %.4f | %.4f | %.4f | %.4f | %.4f | %.4f | %.4f | %.4f | %.4f \n\n", update->ntimestep, ttot, tbefore_particle_map, tparticle_map, tmake_rho, treverse_comm, tbrick2fft, tpoisson, tafter_poisson, tfield_force, tafter_field_force, tbefore_particle_map+tparticle_map+tmake_rho+treverse_comm+tbrick2fft+tpoisson+tafter_poisson+tfield_force+tafter_field_force, tbefore_particle_map/ttot*100, tparticle_map/ttot*100, tmake_rho/ttot*100, treverse_comm/ttot*100, tbrick2fft/ttot*100, tpoisson/ttot*100, tafter_poisson/ttot*100, tfield_force/ttot*100, tafter_field_force/ttot*100, (tbefore_particle_map+tparticle_map+tmake_rho+treverse_comm+tbrick2fft+tpoisson+tafter_poisson+tfield_force+tafter_field_force)/ttot*100);
 }
 
 /* ----------------------------------------------------------------------
